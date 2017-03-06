d8ed92c7c3d07b19384ace8a5a2b9bf65e70a271
JF <thai.jfrancois@gmail.com>
2017-03-06 18:44:01 +0100

    debut OpenMP

--- code/main.c
+++ code/main.c
@@ -1,4 +1,4 @@
-#ifndef _OPENMP
+#ifdef _OPENMP
 #include <omp.h>
 #endif
 #include <time.h>	/* chronometrage */
@@ -18,67 +18,67 @@ double my_gettimeofday(){
 
 void evaluate(tree_t * T, result_t *result)
 {
-        node_searched++;
-  
-        move_t moves[MAX_MOVES];
-        int n_moves;
+	node_searched++;
 
-        result->score = -MAX_SCORE - 1;
-        result->pv_length = 0;
-        
-        if (test_draw_or_victory(T, result))
-          return;
+	move_t moves[MAX_MOVES];
+	int n_moves;
 
-        if (TRANSPOSITION_TABLE && tt_lookup(T, result))     /* la réponse est-elle déjà connue ? */
-          return;
-        
-        compute_attack_squares(T);
+	result->score = -MAX_SCORE - 1;
+	result->pv_length = 0;
+    
+	if (test_draw_or_victory(T, result))
+		return;
 
-        /* profondeur max atteinte ? si oui, évaluation heuristique */
-        if (T->depth == 0) {
-          result->score = (2 * T->side - 1) * heuristic_evaluation(T);
-          return;
-        }
-        
-        n_moves = generate_legal_moves(T, &moves[0]);
+	if (TRANSPOSITION_TABLE && tt_lookup(T, result))     /* la réponse est-elle déjà connue ? */
+		return;
 
-        /* absence de coups légaux : pat ou mat */
+	compute_attack_squares(T);
+
+	/* profondeur max atteinte ? si oui, évaluation heuristique */
+	if (T->depth == 0) {
+		result->score = (2 * T->side - 1) * heuristic_evaluation(T);
+		return;
+	}
+
+	n_moves = generate_legal_moves(T, &moves[0]);
+
+	/* absence de coups légaux : pat ou mat */
 	if (n_moves == 0) {
-          result->score = check(T) ? -MAX_SCORE : CERTAIN_DRAW;
-          return;
-        }
-        
-        if (ALPHA_BETA_PRUNING)
-          sort_moves(T, n_moves, moves);
+		result->score = check(T) ? -MAX_SCORE : CERTAIN_DRAW;
+		return;
+	}
 
-        /* évalue récursivement les positions accessibles à partir d'ici */
-        for (int i = 0; i < n_moves; i++) {
+	if (ALPHA_BETA_PRUNING)
+		sort_moves(T, n_moves, moves);
+
+	/* évalue récursivement les positions accessibles à partir d'ici */
+	for (int i = 0; i < n_moves; i++) {
 		tree_t child;
-                result_t child_result;
-                
-                play_move(T, moves[i], &child);
-                
-                evaluate(&child, &child_result);
-                         
-                int child_score = -child_result.score;
+		result_t child_result;
+
+		play_move(T, moves[i], &child);
+
+		evaluate(&child, &child_result);
+		         
+		int child_score = -child_result.score;
 
 		if (child_score > result->score) {
 			result->score = child_score;
 			result->best_move = moves[i];
-                        result->pv_length = child_result.pv_length + 1;
-                        for(int j = 0; j < child_result.pv_length; j++)
-                          result->PV[j+1] = child_result.PV[j];
-                          result->PV[0] = moves[i];
-                }
+			result->pv_length = child_result.pv_length + 1;
+			for(int j = 0; j < child_result.pv_length; j++)
+				result->PV[j+1] = child_result.PV[j];
+			result->PV[0] = moves[i];
+		}
 
-                if (ALPHA_BETA_PRUNING && child_score >= T->beta)
-                  break;    
+		if (ALPHA_BETA_PRUNING && child_score >= T->beta)
+			break;
 
-                T->alpha = MAX(T->alpha, child_score);
-        }
+		T->alpha = MAX(T->alpha, child_score);
+	}
 
-        if (TRANSPOSITION_TABLE)
-          tt_store(T, result);
+	if (TRANSPOSITION_TABLE)
+		tt_store(T, result);
 }
 
 
@@ -90,14 +90,14 @@ void decide(tree_t * T, result_t *result)
 		T->alpha_start = T->alpha = -MAX_SCORE - 1;
 		T->beta = MAX_SCORE + 1;
 
-                printf("=====================================\n");
+		printf("=====================================\n");
 		evaluate(T, result);
 
-                printf("depth: %d / score: %.2f / best_move : ", T->depth, 0.01 * result->score);
-                print_pv(T, result);
-                
-                if (DEFINITIVE(result->score))
-                  break;
+		printf("depth: %d / score: %.2f / best_move : ", T->depth, 0.01 * result->score);
+		print_pv(T, result);
+
+		if (DEFINITIVE(result->score))
+			break;
 	}
 }
 
@@ -105,24 +105,24 @@ int main(int argc, char **argv)
 {
 	double debut, fin;
 	tree_t root;
-        result_t result;
+	result_t result;
 
-        if (argc < 2) {
-          printf("usage: %s \"4k//4K/4P w\" (or any position in FEN)\n", argv[0]);
-          exit(1);
-        }
+	if (argc < 2) {
+		printf("usage: %s \"4k//4K/4P w\" (or any position in FEN)\n", argv[0]);
+		exit(1);
+	}
 
-        if (ALPHA_BETA_PRUNING)
-          printf("Alpha-beta pruning ENABLED\n");
+	if (ALPHA_BETA_PRUNING)
+		printf("Alpha-beta pruning ENABLED\n");
 
-        if (TRANSPOSITION_TABLE) {
-          printf("Transposition table ENABLED\n");
-          init_tt();
-        }
-        
-        parse_FEN(argv[1], &root);
-        print_position(&root);
-        
+	if (TRANSPOSITION_TABLE) {
+		printf("Transposition table ENABLED\n");
+		init_tt();
+	}
+
+	parse_FEN(argv[1], &root);
+	print_position(&root);
+
 	/* debut du chronometrage */
 	debut = my_gettimeofday();
 
@@ -133,16 +133,16 @@ int main(int argc, char **argv)
 	fprintf( stderr, "Temps total de calcul : %g sec\n", fin - debut);
 
 	printf("\nDécision de la position: ");
-        switch(result.score * (2*root.side - 1)) {
-        case MAX_SCORE: printf("blanc gagne\n"); break;
-        case CERTAIN_DRAW: printf("partie nulle\n"); break;
-        case -MAX_SCORE: printf("noir gagne\n"); break;
-        default: printf("BUG\n");
-        }
+	switch(result.score * (2*root.side - 1)) {
+		case MAX_SCORE: printf("blanc gagne\n"); break;
+		case CERTAIN_DRAW: printf("partie nulle\n"); break;
+		case -MAX_SCORE: printf("noir gagne\n"); break;
+		default: printf("BUG\n");
+	}
 
-        printf("Node searched: %llu\n", node_searched);
-        
-        if (TRANSPOSITION_TABLE)
-          free_tt();
+	printf("Node searched: %llu\n", node_searched);
+
+	if (TRANSPOSITION_TABLE)
+		free_tt();
 	return 0;
 }
